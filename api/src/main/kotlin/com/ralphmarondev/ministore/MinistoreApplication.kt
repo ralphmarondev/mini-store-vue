d140fb732e3aaf7811458f2078d9ea1d5ab4597b
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-14 00:39:01 +0800

    api: base commit for api

---
+++ api/src/main/kotlin/com/ralphmarondev/ministore/MinistoreApplication.kt
@@ -0,0 +1,11 @@
+package com.ralphmarondev.ministore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinistoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinistoreApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/ralphmarondev/ministore/MinistoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ralphmarondev.ministore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinistoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
